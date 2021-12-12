1ba14a2a1628485c7046e8dd6522f894ab175da5
kimchansoo <kimchansoo@gimchansuui-MacBookPro.local>
2021-12-12 23:58:17 +0900

    complete api except 3api

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,34 @@ export const userRouter = express.Router();
 
 userRouter.use(isLoggedIn, verifyToken, apiLimiter);
 
-userRouter.get("/user/:id",  )
+userRouter.get("/:id", async(req, res) =>{
+    const user = User.findOne({where: {userId: req.params.id}});
+    return res.json({
+        code: 200,
+        payload: user,
+    });
+});
+
+userRouter.get("/following/:id/", async(req, res)=>{
+    const user = User.findOne({where: {userId: req.params.id}});
+    const followingUser = user.getFollowings();
+    return res.status(200).json(followingUser);
+})
+
+userRouter.get("/followed/:id/", async(req, res)=>{
+    const user = User.findOne({where: {userId: req.params.id}});
+    const followedUser = user.getFollowers();
+    return res.status(200).json(followedUser);
+})
+
+userRouter.get("/like/:id", async(req, res)=>{
+    const likePost = req.params.id.
+})
+
+userRouter.get("/bookmark/:id", async(req, res)=>{
+
+})
+
+userRouter.post("/follow/:id", async(req, res)=>{
+    
+})
